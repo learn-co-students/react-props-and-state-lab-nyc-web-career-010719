4a642ac08478d5377e76a128fe8387bdfbd24aed
github username <github email address>
2019-03-14 20:38:53 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,39 @@ class App extends React.Component {
     }
   }
 
+  fetchPets = () => {
+    // debugger
+    console.log("finding")
+    let param = this.state.filters.type
+    let url
+
+    if(param === "all"){
+      url = `/api/pets`
+    }
+    else {url = `/api/pets?type=${param}`}
+
+    fetch(url)
+    .then(r => r.json())
+    .then(fetchedPets => {
+      this.setState({
+        pets: fetchedPets
+      },() => console.log(this.state.pets))
+    })
+  }
+
+  handleChangeType = ({ target: { value } }) => {
+   this.setState({ filters: { ...this.state.filters, type: value } });
+  };
+
+  onAdoptPet = petId => {
+    const pets = this.state.pets.map(p => {
+     return p.id === petId ? { ...p, isAdopted: true } : p
+    })
+   this.setState({ pets })
+  }
+
+
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +57,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.handleChangeType} onFindPetsClick={this.fetchPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,11 +1,23 @@
 import React from 'react'
-
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
-  render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+
+  renderPets = (pets) => {
+    console.log(pets)
+    console.log("we are here")
+    // debugger
+    return pets.map(p => <Pet key={p.id} onAdoptPet={this.props.onAdoptPet} pet={p} isAdopted={p.isAdopted}/>)
   }
+
+  render() {
+    return (
+      <div
+        className="ui cards"
+      >
+        {this.renderPets(this.props.pets)}
+      </div>
+  )}
 }
 
 export default PetBrowser
